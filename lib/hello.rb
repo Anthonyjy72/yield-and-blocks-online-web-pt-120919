e0af63dec38b3ec06c80a862f0beaeb38ce4c19b
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-21 08:00:55 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,14 @@
-def hello_t
-  i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-  array
-end
-
-hello_t(["Tim", "Tom", "Jim"])
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
